2a19f767cb1962323aad65434ff4f600d4f424a0
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2023-07-29 19:24:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { DataStore } from "@aws-amplify/datastore";
+import { Users } from "./models";
+import { useState } from "react";
 
 function App() {
+  const [data, setdata] = useState();
+  const postData = async () => {
+    await DataStore.save(
+      new Users({
+        name: "The Admin Guy",
+        college_level: "Senior",
+        email: "zecharydouglas@testemail.com",
+        password: "adfadf",
+        college: "Cuny College of Staten Island",
+        major: "Computer Science",
+        course_interests: "Data Structures",
+      })
+    );
+  };
+
+  const fetchData = async () => {
+    const newData = await DataStore.query(Users);
+    setdata(newData);
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hello</div>
+
+      <button onClick={postData}>Post</button>
+
+      <button onClick={fetchData}>Fetch</button>
     </div>
   );
 }
